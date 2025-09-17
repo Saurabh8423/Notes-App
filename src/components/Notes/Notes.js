dd1e8b005c43a5e7f2523d54a2e844a6739a0ef8
saurabh8423 <saurabhch9369@gmail.com>
2025-09-17 20:04:51 +0530

    Added Notes making pages and styles

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import NoteItem from "../NoteItem/NoteItem";
+import {addNoteToGroup} from "../../utils/storage";
+import "./Notes..css"
 
-const Notes = () => {
+const Notes = ({groupName, notes, refreshNotes}) => {
+  const [input, setInput] = useState("");
+
+  const handleAdd =()=>{
+    if (!input.trim()) return;
+    addNoteToGroup(groupName, input);
+    setInput('');
+    refreshNotes();
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="notes">
+      <h2>{groupName}</h2>
+      <div className="notes-list">
+        {notes.map((note) => (
+          <NoteItem key={note.id} note={note} group={groupName} />
+        ))}
+      </div>
 
-export default Notes
+      <div className="note-input">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleAdd()}
+          placeholder="Type Your note..."
+        />
+        <button onClick={handleAdd} disabled={!input.trim()}>
+          ➤
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Notes;
