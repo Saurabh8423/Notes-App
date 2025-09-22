6ae5f6ba543f73889189f83c2b2faca9d333dbe1
saurabh8423 <saurabhch9369@gmail.com>
2025-09-22 13:21:20 +0530

    updated some mobile View Styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Sidebar from "../components/Sidebar/Sidebar.js"
+import Sidebar from "../components/Sidebar/Sidebar.js";
 import GroupPopup from "../components/GroupPopup/GroupPopup.js";
 import Notes from "../components/Notes/Notes.js";
 import homeImage from "../assets/welcome.png";
@@ -21,22 +21,20 @@ function Home() {
   };
 
   const handleBack = () => {
-    setSelectedGroup(null); 
+    setSelectedGroup(null);
   };
-
 
   return (
     <div className="container">
       <div className={`sidebar-wrapper ${selectedGroup ? "hide-mobile" : ""}`}>
-      <Sidebar
-        groups={groups}
-        onSelectGroup={setSelectedGroup}
-        onAddGroup={() => setShowPopup(true)}
-      />
+        <Sidebar
+          groups={groups}
+          onSelectGroup={setSelectedGroup}
+          onAddGroup={() => setShowPopup(true)}
+        />
       </div>
 
       <div className={`notesArea ${!selectedGroup ? "hide-mobile" : ""}`}>
-
         {selectedGroup ? (
           <Notes group={selectedGroup} onBack={handleBack} />
         ) : (
@@ -44,18 +42,22 @@ function Home() {
             <img className="homeImages" src={homeImage} alt="Pocket Notes" />
             <h1>Pocket Notes</h1>
             <p>
-              Send and receive messages without keeping your phone online. <br /> Use
-              Pocket Notes on up to 4 linked devices and 1 mobile phone.
+              Send and receive messages without keeping your phone online.{" "}
+              <br /> Use Pocket Notes on up to 4 linked devices and 1 mobile
+              phone.
             </p>
             <small className="encrypt">🔒end-to-end encrypted</small>
           </div>
         )}
       </div>
-      {showPopup && <GroupPopup closePopup={() => setShowPopup(false)}
-        onCreate={handleCreateGroup}
-      />}
+      {showPopup && (
+        <GroupPopup
+          closePopup={() => setShowPopup(false)}
+          onCreate={handleCreateGroup}
+        />
+      )}
     </div>
   );
-};
+}
 
 export default Home;
